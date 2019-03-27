c9491bfa36a73afcb055ea9c6c345b3c85679c82
macjabeth <macbethjonathan@gmail.com>
2019-03-27 22:18:27 +0000

    setup default configuration

--- data/knexfile.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  development: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: { filename: './lambda.sqlite3' },
-    seeds: { directory: './seeds' }
-  }
-};
--- data/migrations/20190327210338_add_cohorts_table.js
+++ data/migrations/20190327210338_add_cohorts_table.js
@@ -3,6 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('cohorts', (tbl) => {
     tbl.increments();
     tbl.string('name').notNullable().unique();
+    tbl.timestamp('createdAt').defaultTo(knex.fn.now());
   });
 };
 
